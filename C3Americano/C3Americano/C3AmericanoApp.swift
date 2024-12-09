76f8eab6919198c118eb800e049b0fc326f4d129
Fedhirweg <avahmethaydarisik@gmail.com>
2024-12-09 16:36:43 +0100

    Initial commit on xcode

---
+++ C3Americano/C3Americano/C3AmericanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  C3AmericanoApp.swift
+//  C3Americano
+//
+//  Created by Ahmet Haydar ISIK on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct C3AmericanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
