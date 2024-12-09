21382bdbecd65146c9dea4f73bba4f839381e4bd
Fedhirweg <avahmethaydarisik@gmail.com>
2024-12-09 22:29:36 +0100

    Added the text input companent i will use during forms for registration and login

---
+++ C3Americano/C3Americano/Companents/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  C3Americano
+//
+//  Created by Ahmet Haydar ISIK on 9/12/24.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
